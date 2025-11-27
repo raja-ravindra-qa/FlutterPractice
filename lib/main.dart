3a7f1deb9db70df2417ddf5ba9bdac2f878b2c3b
raja-ravindra-qa <raja.ravindra.qa@gmail.com>
2025-11-27 19:26:57 +0530

    text style was introduced

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,17 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: AlignmentGeometry.topLeft,
-              end: AlignmentGeometry.bottomRight,
-              colors: [
-                const Color.fromARGB(255, 33, 49, 230),
-                const Color.fromARGB(255, 3, 144, 34),
-              ],
-            ),
+                body: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  begin: AlignmentGeometry.topLeft,
+                  end: AlignmentGeometry.bottomRight, colors: [const Color.fromARGB(255, 27, 20, 159), const Color.fromARGB(255, 25, 233, 32)]),
+                  ),
+                  child: Center(
+                            child: Text('Hello World!',style: TextStyle(color: Colors.white,fontSize: 32),),
+                            ),
+                ),
           ),
-          child: Center(child: Text('Hello World!')),
-        ),
-      ),
     ),
   );
 }
