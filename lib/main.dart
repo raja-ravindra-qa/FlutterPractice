1dd4880ac2e0b45749e93bb46e588b3d0d05195e
raja-ravindra-qa <raja.ravindra.qa@gmail.com>
2025-11-27 17:35:59 +0530

    organized the screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main(){
-runApp(const MaterialApp(home: Text('Hello World!')));
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor:Color.fromARGB(255, 58, 5, 100),
+                body: Center(
+          child: Text('Hello World!'),
+          ),
+          ),
+    ),
+  );
 }
