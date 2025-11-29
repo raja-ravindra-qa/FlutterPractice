a324a648e45c0aee5096b9caaba0e43e5fe35267
raja-ravindra-qa <raja.ravindra.qa@gmail.com>
2025-11-30 04:53:27 +0530

    Classes introduced

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,36 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-                body: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                  begin: AlignmentGeometry.topLeft,
-                  end: AlignmentGeometry.bottomRight, colors: [const Color.fromARGB(255, 27, 20, 159), const Color.fromARGB(255, 25, 233, 32)]),
-                  ),
-                  child: Center(
-                            child: Text('Hello World!',style: TextStyle(color: Colors.white,fontSize: 32),),
-                            ),
-                ),
-          ),
+        body: Gradient_Container()
+      ),
     ),
   );
+}
+
+class Gradient_Container extends StatelessWidget {
+  // const Gradient_Container({super.key});
+
+
+  @override
+  Widget build(context) {
+    return Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: AlignmentGeometry.topLeft,
+              end: AlignmentGeometry.bottomRight,
+              colors: [
+                const Color.fromARGB(255, 27, 20, 159),
+                const Color.fromARGB(255, 25, 233, 32),
+              ],
+            ),
+          ),
+          child: Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(color: Colors.white, fontSize: 32),
+            ),
+          ),
+        );
+    
+  }
 }
